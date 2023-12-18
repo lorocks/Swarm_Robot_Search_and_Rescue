5758565553f1602158b99665973b9e6b4242570c
lorocks <lowell.lobo@hotmail.com>
2023-12-18 15:55:06 -0500

    Custom launch file
    This commit customises the launch file to control all actions

--- src/multi_robot/launch/gazebo.launch.py
+++ src/multi_robot/launch/gazebo.launch.py
@@ -16,19 +16,59 @@ def generate_launch_description():
     robot_num = DeclareLaunchArgument(name="robot_num", default_value='2')
 
     launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
 
     n = 5
     x_pose = LaunchConfiguration('x_pose', default=f'{n}')
     y_pose = LaunchConfiguration('y_pose', default='-0.5')
-    spawn_turtlebot = IncludeLaunchDescription(
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    world = os.path.join(
+        get_package_share_directory('turtlebot3_gazebo'),
+        'worlds',
+        'empty_world.world'
+    )
+
+    gzserver_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(launch_file_dir, 'turtlebot3_house.launch.py')
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world}.items()
+    )
+
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+        )
+    )
+
+
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')
+        ),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+
+    spawn_turtlebot_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
         ),
         launch_arguments={
             'x_pose': x_pose,
             'y_pose': y_pose
         }.items()
     )
+
+    ld = LaunchDescription()
+
+    # Add the commands to the launch description
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+    ld.add_action(robot_state_publisher_cmd)
+    ld.add_action(spawn_turtlebot_cmd)
+
+    return ld
 
 
     desc.add_action(spawn_turtlebot)

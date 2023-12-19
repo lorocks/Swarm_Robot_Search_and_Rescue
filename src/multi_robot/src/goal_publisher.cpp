c1d7ea1f4c00596e3a95a61a13cb675c5b4d0a31
Mayank Deshpande <deshpandemayank5@gmail.com>
2023-12-19 17:40:19 -0500

    Added support for multiple robots

--- src/multi_robot/src/goal_publisher.cpp
+++ src/multi_robot/src/goal_publisher.cpp
@@ -8,27 +8,42 @@
 class NavigateToPoseNode : public rclcpp::Node
 {
 public:
-    NavigateToPoseNode()
-        : Node("navigate_to_pose_client"), goal_index(0)
+    NavigateToPoseNode(const std::string& robot_namespace)
+        : Node("navigate_to_pose_client_" + robot_namespace), present_goal(0), robot_namespace_(robot_namespace)
     {
         // Create an ActionClient for the NavigateToPose action
-        goal_send = rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(this, "/navigate_to_pose");
+        goal_send = rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(this, "/" + robot_namespace + "/navigate_to_pose");
 
         // Wait for the action server to be available
         if (!goal_send->wait_for_action_server(std::chrono::seconds(10)))
         {
-            RCLCPP_ERROR(get_logger(), "Action server not available");
+            RCLCPP_ERROR(get_logger(), "Action server not available for robot %s", robot_namespace.c_str());
             return;
         }
 
-        // Define a list of GoalPositions (x, y)
-        goal_positions = {
-            {0.5, 1.8},
-            {0.21, -1.8},
-            {0.5, 1.8},
-            {0.21, -1.8},
-            {0.5, 1.8}
-        };
+        // Create a list of GoalPositions for each robot
+        if (robot_namespace == "tb1")
+        {
+            // goals_list = generateGoalListRobot1();
+            goals_list = {
+                {0.5, 1.8},
+                {0.21, -1.8},
+                {0.5, 1.8},
+                {0.21, -1.8},
+                {0.5, 1.8}
+            };
+        }
+        else if (robot_namespace == "tb2")
+        {
+            // goals_list = generateGoalListRobot2();
+            goals_list = {
+                {1.87, 0.902},
+                {-0.87, 2.1},
+                {1.87, 0.902},
+                {-0.87, 2.1},
+                {1.87, 0.902}
+            };
+        }
 
         // Send the first goal
         sendNextGoal();
@@ -37,13 +52,13 @@ public:
 private:
     void sendNextGoal()
     {
-        if (goal_index < goal_positions.size())
+        if (present_goal < goals_list.size())
         {
             // Create a goal for the NavigateToPose action with constant z and orientation values
             auto goal_msg = nav2_msgs::action::NavigateToPose::Goal();
             goal_msg.pose.header.frame_id = "map";
-            goal_msg.pose.pose.position.x = goal_positions[goal_index].x;
-            goal_msg.pose.pose.position.y = goal_positions[goal_index].y;
+            goal_msg.pose.pose.position.x = goals_list[present_goal].x;
+            goal_msg.pose.pose.position.y = goals_list[present_goal].y;
             goal_msg.pose.pose.position.z = 0.0;  // Constant z value
             goal_msg.pose.pose.orientation.x = 0.0;
             goal_msg.pose.pose.orientation.y = 0.0;
@@ -55,11 +70,11 @@ private:
             send_goal_options.result_callback = std::bind(&NavigateToPoseNode::result_callback, this, std::placeholders::_1);
             auto future_goal_handle = goal_send->async_send_goal(goal_msg, send_goal_options);
 
-            RCLCPP_INFO(get_logger(), "Sending goal %zu", goal_index);
+            RCLCPP_INFO(get_logger(), "[%s] Sending goal %zu", robot_namespace_.c_str(), present_goal);
         }
         else
         {
-            RCLCPP_INFO(get_logger(), "All goals achieved!");
+            RCLCPP_INFO(get_logger(), "[%s] All goals achieved!", robot_namespace_.c_str());
         }
     }
 
@@ -68,30 +83,54 @@ private:
         switch (result.code)
         {
         case rclcpp_action::ResultCode::SUCCEEDED:
-            RCLCPP_INFO(get_logger(), "Goal %zu succeeded!", goal_index);
+            RCLCPP_INFO(get_logger(), "[%s] Goal %zu succeeded!", robot_namespace_.c_str(), present_goal);
 
             // Move to the next goal
-            goal_index++;
+            present_goal++;
             sendNextGoal();
             break;
         case rclcpp_action::ResultCode::ABORTED:
-            RCLCPP_ERROR(get_logger(), "Goal %zu was aborted", goal_index);
+            RCLCPP_ERROR(get_logger(), "[%s] Goal %zu was aborted", robot_namespace_.c_str(), present_goal);
 
             // Retry the same goal on failure
             sendNextGoal();
             break;
         default:
-            RCLCPP_ERROR(get_logger(), "Unknown result code");
+            RCLCPP_ERROR(get_logger(), "[%s] Unknown result code", robot_namespace_.c_str());
 
-            // Move to the next goal
-            goal_index++;
+            present_goal++;
             sendNextGoal();
             break;
         }
     }
 
-    std::vector<GoalPosition> goal_positions;
-    size_t goal_index;
+    std::vector<GoalPosition> generateGoalListRobot1()
+    {
+        // Create a list of GoalPositions with random values for robot 1
+        std::vector<GoalPosition> goals;
+        GoalGenerator goal_generator(1, 1);
+        for (int i = 0; i < 5; ++i) // i is the number of goals to be generated
+        {
+            goals.push_back(goal_generator.generateRandomGoal());
+        }
+        return goals;
+    }
+
+    std::vector<GoalPosition> generateGoalListRobot2()
+    {
+        // Create a list of GoalPositions with random values for robot 2
+        std::vector<GoalPosition> goals;
+        GoalGenerator goal_generator(1, 1); 
+        for (int i = 0; i < 5; ++i) // i is the number of goals to be generated
+        {
+            goals.push_back(goal_generator.generateRandomGoal());
+        }
+        return goals;
+    }
+
+    std::vector<GoalPosition> goals_list;
+    size_t present_goal;
+    std::string robot_namespace_;
     rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SharedPtr goal_send;
 };
 
@@ -99,8 +138,8 @@ int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
 
-    // Create an instance of NavigateToPoseNode
-    auto node = std::make_shared<NavigateToPoseNode>();
+    // Create an instance of NavigateToPoseNode with the provided robot namespace
+    auto node = std::make_shared<NavigateToPoseNode>(argv[1]);
 
     auto spinner = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
     spinner->add_node(node);
@@ -117,4 +156,5 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
 

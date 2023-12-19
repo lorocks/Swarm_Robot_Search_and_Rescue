9661b5de19e2b71a6f6160747482bc310cb3810d
lorocks <lowell.lobo@hotmail.com>
2023-12-18 22:51:01 -0500

    Add n number of robot spawns
    This commit changes the presetup robot numbers spawned to user input

--- src/multi_robot/launch/multi_robot_spawn.launch.py
+++ src/multi_robot/launch/multi_robot_spawn.launch.py
@@ -9,31 +9,19 @@ from launch.event_handlers import OnProcessExit
 from ament_index_python.packages import get_package_share_directory
 import os
 
-n = 2
+
+ld = LaunchDescription()
 
 def get_num(context: LaunchContext, robot_num):
     num_str = context.perform_substitution(robot_num)
-    n = int(num_str)
-    print("done")
-
-"""@brief Generate the launch descriptions for ROS
-"""
-def generate_launch_description():
-    robot_num_arg = DeclareLaunchArgument('robot_num', default_value='2')
-    robot_num = LaunchConfiguration("robot_num", default='2')
-
-    function = OpaqueFunction(function=get_num, args=[LaunchConfiguration('robot_num')])
-
-    launch_file_dir = os.path.join(get_package_share_directory('multi_robot'), 'launch')
-
     spawn_robots = []
-    for i in range(n):
+    for i in range(int(num_str)):
         x_pose = LaunchConfiguration('x_pose', default=f'{3*i}')
         y_pose = LaunchConfiguration('y_pose', default='0.0')
         spawn_robots.append(
                 IncludeLaunchDescription(
                 PythonLaunchDescriptionSource(
-                    os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
+                    os.path.join(os.path.join(get_package_share_directory('multi_robot'), 'launch'), 'spawn_turtlebot3.launch.py')
                 ),
                 launch_arguments={
                     'x_pose': x_pose,
@@ -42,30 +30,19 @@ def generate_launch_description():
                 }.items()
             )
         )
+    for robot in spawn_robots:
+        ld.add_action(robot)
     
-    # spawn_robots = [ IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource(
-    #                 os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
-    #             ),
-    #             launch_arguments={
-    #                 'x_pose': f'{i}',
-    #                 'y_pose': '0.0',
-    #                 'robot_prefix': f'tb{i}',
-    #             }.items()
-    #         ) for i in range(n)]
 
-    # delay_spawnning = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=function,
-    #         on_exit=[spawn_robots],
-    #     )
-    # )
+"""@brief Generate the launch descriptions for ROS
+"""
+def generate_launch_description():
+    robot_num_arg = DeclareLaunchArgument('robot_num', default_value='2')
+    robot_num = LaunchConfiguration("robot_num", default='2')
 
-    ld = LaunchDescription()
+    function = OpaqueFunction(function=get_num, args=[LaunchConfiguration('robot_num')])
     
     ld.add_action(robot_num_arg)
     ld.add_action(function)
-    for robot in spawn_robots:
-        ld.add_action(robot)
 
     return ld
